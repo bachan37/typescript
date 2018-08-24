a9409724420c2523b81c8e19a8a7cdb8c12fd475
bachan37 <bachan37@gmail.com>
2018-08-24 13:31:58 +0530

    initial commit with hello_world

---
+++ 000_hello_world/hello_world.ts
@@ -0,0 +1,13 @@
+/**
+* This is the first step.
+* Need to create a folder hello_world 
+* Add hello_world.ts ts stands for typescript.
+* hope you have npm and node installed in your system
+* npm install -g typescript
+* cd hello_world
+* tsc --init // this creates tsconfig.json
+* After writing and configuring the stuffs, you need to compile
+* tsc // this will compile and create the js file from ts file.
+**/
+
+console.log("Hello World")
